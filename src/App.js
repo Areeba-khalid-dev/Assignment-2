ec9b0bf86765a3890261100b9c5e678dd81d367d
khizar872 <201370132@gift.edu.pk>
2023-08-25 23:31:23 +0500

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Areeba Khalid</h1>
+        <h1>201370132</h1>
       </header>
     </div>
   );
